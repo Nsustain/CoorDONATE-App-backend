9cd131b95bf8d79a41428f356f271eff5bb91c17
Stargazing-11 <samuelkifle199314@gmail.com>
2023-09-04 22:04:15 +0300

    update on chat side of chat feature

--- src/routes/chat.routes.ts
+++ src/routes/chat.routes.ts
@@ -2,9 +2,11 @@ import express from "express";
 import deserializeUser from "../middleware/deserializeUser";
 import requireUser from "../middleware/requireUser";
 import { chatController } from "../controllers/chat.controller";
+import { getChatsByUser } from '../middleware/getChatByUser';
 
 const chatRouter = express.Router()
 
 chatRouter.use(deserializeUser, requireUser);
 
-chatRouter.post('/create', chatController.create)
+chatRouter.post('/create', chatController.create);
+chatRouter.get('/get:userId', getChatsByUser, chatController.getAllChats);
