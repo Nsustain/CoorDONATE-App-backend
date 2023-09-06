0829637fc9b0c13a38a512491849a457481acad9
aklile-yilma <aklileyilma@gmail.com>
2023-09-06 22:43:28 +0300

    fix(tobe): to be implemented

---
+++ src/websockets/index.ts
@@ -0,0 +1,24 @@
+
+
+
+// class SocketController{
+
+//     protected io: server;
+//     construtor(Server io){
+    //  this.io = io
+//     io.on("connection", (Socket socket){
+//     this.mapEvents();
+//     }
+//     }
+    
+//     private mapEvents(){
+//    ( io.on"chat message", onChat)
+//     }
+    
+//     private onChat(Socket socekt){
+//         do something here
+    
+//     }
+//     }
+
+
