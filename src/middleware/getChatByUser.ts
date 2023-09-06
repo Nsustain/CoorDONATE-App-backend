d0094383a4020cc8986c9b76abb9a52a0a6060b4
aklile-yilma <aklileyilma@gmail.com>
2023-09-06 09:58:31 +0300

    feat(chat): getchats by userid

--- src/middleware/getChatByUser.ts
+++ src/middleware/getChatByUser.ts
@@ -1,17 +1,21 @@
 import { Request, Response, NextFunction } from 'express';
 import { ChatRoom } from '../entities/chat.entity'; // Import the Chat entity
 import { User } from '../entities/user.entity'; // Import the User entity
-import { Repository } from 'typeorm';
 import AppDataSource from '../config/ormconfig';
+import AppError from '../utils/appError';
+import { findChatByUserId } from '../services/chat.service';
 
 const userRepository = AppDataSource.getRepository(User);
 const chatRepository = AppDataSource.getRepository(ChatRoom);
 
 export async function getChatsByUser(req: Request, res: Response, next: NextFunction) {
-  console.log("I am here mothafuker")
-
   try {
     const { userId } = req.params;
+
+    // check authority
+    if (userId !== res.locals.user.id.toString()){
+      return next(new AppError(401, "You are not authorized to view this chats."))
+    }
 
     // Fetch the user based on the provided userId
     const user = await userRepository.findOneBy({id: userId});
@@ -21,12 +25,7 @@ export async function getChatsByUser(req: Request, res: Response, next: NextFunc
     }
 
     // Fetch chats where the user is a member
-    const chats = await chatRepository
-      .createQueryBuilder('chat')
-      .innerJoinAndSelect('chat.members', 'member')
-      .where('member.id = :userId', { userId: user.id })
-      .getMany();
-
+    const chats = findChatByUserId(user.id)
     // Attach the chats to res.locals
     res.locals.chats = chats;
 
