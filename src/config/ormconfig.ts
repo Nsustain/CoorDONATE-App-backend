806c1f8480cbc3ce239d656d5cded3da245ca090
aklile-yilma <aklileyilma@gmail.com>
2023-09-18 11:58:39 +0300

    fix(ormconfig)

--- src/config/ormconfig.ts
+++ src/config/ormconfig.ts
@@ -1,34 +1,23 @@
 import 'reflect-metadata';
 import { DataSource } from 'typeorm';
-import UserSession from '../entities/user.session.ts';
-import { User } from '../entities/user.entity.ts';
-import { Post, PostImage, Comment, Like } from '../entities/post.entity.ts';
-import { ChatRoom } from '../entities/chat.entity.ts';
-import { Message } from '../entities/message.entity.ts';
-import { Profile } from '../entities/profile.entity.ts';
+import UserSession from '../entities/user.session';
+import { User } from '../entities/user.entity';
+import { Post, PostImage, Comment, Like } from '../entities/post.entity';
+import { ChatRoom } from '../entities/chat.entity';
+import { Message } from '../entities/message.entity';
+import { Profile } from '../entities/profile.entity';
 import { Notification } from '../entities/notification.entity';
 
 const AppDataSource = new DataSource({
   type: 'postgres',
-  host: 'localhost',
+  host: "surus.db.elephantsql.com",
   port: 5432,
-  username: 'coordev',
-  password: '12345678',
-  database: 'coordonate',
+  username: "puwogwoj",
+  password: "LJmbehYSUhRbhj_omsYAd0MuAKj8aSKz",
+  database: "puwogwoj",
   synchronize: true,
   logging: true,
-  entities: [
-    User,
-    UserSession,
-    Post,
-    PostImage,
-    Comment,
-    Like,
-    ChatRoom,
-    Message,
-    Profile,
-    Notification,
-  ],
+  entities: [User, UserSession, Post, PostImage, Comment, Like, ChatRoom, Message, Profile, Notification],
   subscribers: [],
   migrations: [],
 });
